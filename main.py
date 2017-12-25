79db81ccb3fc4f3c1bda3c84b98056e0c0094570
= <alexander.mcvittie@gmail.com>
2017-12-25 01:19:22 -0500

    End of evening commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-## This program's main functionality is to auto fetch the newest Strava routes from your profile and save them as GeoJSON files.
+## This program's main functionality is to auto fetch the newest Strava routes from your profile and append them to a GeoJSON file.
 ## Licensed under the GNU GPL v3.0.
 ## Written December 2016 by Alex McVittie
 ##
@@ -17,3 +17,12 @@ from stravalib.client import Client
 
 def main():
     pass
+
+## update_geojson: String String --> None
+## Purpose:
+##      Takes in the path to an input GPX track file and appends the GPX track data to a target geoJSON file
+def update_geojson(in_gpx, target_geojson):
+    # Convert GPX to a temporary geojson file - will be appended in second step to target geojson
+    ogr2ogr.main(["", "-f", "geoJSON", in_gpx,"tracks",   "tmp.geoJSON", "-fieldTypeToString", "DateTime"])
+
+
