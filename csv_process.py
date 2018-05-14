477da644bd193e9a7f0c0e052309c19d42e9251e
Alex McVittie <alex@nsahq.uwaterloo.ca>
2018-05-13 23:39:43 -0400

    Tested and confirmed to work appending data to a geojson from the CSV.

--- csv_process.py
+++ csv_process.py
@@ -1,47 +1,62 @@
 #!/usr/bin/env python3
 
-import os, sys, glob
+import os, sys, glob, json
 csv_dir = os.getcwd()
 csvs = glob.glob(os.path.join(csv_dir, "*.csv"))
 # Iterate through directory of csv files
 for a_csv in csvs:
-	# Check if this csv is in the list of csv files processed
-	try:
-		f = open(os.path.join(csv_dir, "processed.txt"), 'r')
-		f = f.read().split("\n")
-		if a_csv in f:
-			del f
-			continue
-		del f
-	except:
-		pass
-	current_dataset = open(a_csv, 'r')
-	current_dataset_r = current_dataset.read().split("\n")
-	activityid = current_dataset_r[0].index("act_id")
-	try:
-		lastid = current_dataset_r[1].split(",")[activityid]
-	except:
-		continue
-	poly_points = {}
-	poly_points[lastid] = []
-	for l in current_dataset_r[1:]:
-		# Get the point info for the current polyline
-		l = l.split(",")
-		lat, lon = l[-2:]
-		id = l[activityid]
-		if id != lastid:
-			lastid = id
-			# End this polyline and start a new polyline
-			poly_points[id] = []
-			poly_points[id].append([lat, lon, 0.0])
-		else:
-			poly_points[id].append([lat, lon, 0.0])
-	existing_geojson = open("strava.geojson", "r")
-	existing_geojson_r = existing_geojson.read()
-	
-	# Write out that we have processed this CSV file so that 
-	# on next run, we do not have it repeat process.
-	f = open(os.path.join(csv_dir, "processed.txt"), 'a')
-	f.write(a_csv)
-	f.write("\n")
-	f.close()
+        # Check if this csv is in the list of csv files processed
+        try:
+                f = open(os.path.join(csv_dir, "processed.txt"), 'r')
+                f = f.read().split("\n")
+                if a_csv in f:
+                        del f
+                        continue
+                del f
+        except:
+                pass
+        current_dataset = open(a_csv, 'r')
+        current_dataset_r = current_dataset.read().split("\n")
+        activityid = current_dataset_r[0].split(",").index("act_id")
+        print(activityid)
+        print(current_dataset_r[1].split(","))
+        lastid = current_dataset_r[1].split(",")[activityid]
+        print(lastid)
+        poly_points = {}
+        poly_points[lastid] = []
+        for l in current_dataset_r[1:]:
+                # Get the point info for the current polyline
+                l = l.split(",")
+                try:
+                    lat, lon = [float(n) for n in l[-2:]]
+                except:
+                    continue
+                id = l[activityid]
+                if id != lastid:
+                        lastid = id
+                        # End this polyline and start a new polyline
+                        poly_points[id] = []
+                        poly_points[id].append([lon, lat])
+                else:
+                        poly_points[id].append([lon, lat])
+        with open('strava.geojson') as strava_j:
+            data = json.load(strava_j)
+        flags = {}
+        for an_id in poly_points.keys():
+            for f in data["features"]:
+                if f["geometry"]["coordinates"] == poly_points[an_id]:
+                    flags[an_id] = True
+                    break
+            if an_id not in flags.keys():
+                data["features"].append({"type": "Feature",
+                    "properties": { "id": "null"},
+                    "geometry": { "type": "LineString", "coordinates": poly_points[an_id] }})
+        with open('strava.geojson', 'w') as fp:
+            json.dump(data, fp)
+
+        # Write out that we have processed this CSV file so that
+        # on next run, we do not have it repeat process.
+        f = open(os.path.join(csv_dir, "processed.txt"), 'a')
+        f.write(a_csv)
+        f.write("\n")
+        f.close()
