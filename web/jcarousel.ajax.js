575eccaa3820c3513001ff788b19dee481c1bc66
corconut <39305874+corconut@users.noreply.github.com>
2018-05-16 11:18:16 -0400

    Added carousel skeleton to instafeed
    Got the basic carousel skeleton working on the instafeed, will add styles later

---
+++ web/jcarousel.ajax.js
@@ -0,0 +1,45 @@
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        var setup = function(data) {
+            var html = '';
+
+            $.each(data.items, function() {
+                html += '<a href="' + this.href + '<img src="' + this.src + '"></a>';
+            });
+
+            // Append items
+            jcarousel
+                .html(html);
+
+            // Reload carousel
+            jcarousel
+                .jcarousel('reload');
+        };
+
+        $.getJSON('data.json', setup);
+    });
+})(jQuery);
